08a6fb26e153726a675d433c61e59dfb69397e04
balthazarely <balthazar.elyj@gmail.com>
2023-02-24 17:00:18 -0800

    more

--- src/api/getGeoCords.ts
+++ src/api/getGeoCords.ts
@@ -1,7 +1,7 @@
 export const getGeoCords = async (query: string) => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
     );
     const data = await response.json();
     return data;
