5e35a8f4f575ffb3352839e47f656c774fd0f686
balthazarely <balthazar.elyj@gmail.com>
2023-02-26 15:13:36 -0800

    another

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ interface iLayout {
 
 export default function Layout({ children }: iLayout) {
   return (
-    <div className=" overflow-x-hidden">
+    <div className=" overflow-hidden bg-gradient-to-tr from-weatherBlue  to-weatherCyan min-h-screen flex-col flex p-2 justify-start sm:justify-center items-center relative">
       {/* <div className="bg-gradient-to-tr from-weatherBlue  to-weatherCyan min-h-screen flex-col flex p-10 justify-start sm:justify-center items-center relative overflow-x-hidden"> */}
       {children}
     </div>
