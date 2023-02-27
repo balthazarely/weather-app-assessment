4ad472277b56551d0e7b8a2ec580f53e8ab67480
balthazarely <balthazar.elyj@gmail.com>
2023-02-27 11:10:58 -0800

    change in manifiest etc

--- src/components/TempToggle.tsx
+++ src/components/TempToggle.tsx
@@ -5,7 +5,7 @@ interface iTempToggle {
 
 export default function TempToggle({ isCelsius, setIsCelsius }: iTempToggle) {
   return (
-    <div className=" w-[60px] relative ">
+    <div className="w-[60px] relative ">
       <button
         className="z-0 h-[25px] w-[55px] rounded-full bg-textblue flex items-center transition duration-300  shadow"
         onClick={() => setIsCelsius(!isCelsius)}
@@ -17,20 +17,20 @@ export default function TempToggle({ isCelsius, setIsCelsius }: iTempToggle) {
           } w-[21px] h-[21px] relative rounded-full transition duration-200 transform bg-white p-1 text-black flex justify-center items-center`}
         ></div>
       </button>
-      <div className="z-50 pointer-events-none  absolute top-0 h-[25px] w-full left-0 flex justify-center gap-[13px] items-center  text-black text-sm   ">
+      <div className="z-50 pointer-events-none  absolute top-0 h-[25px] w-full left-0 flex justify-center gap-[19px] items-center  text-black text-sm   ">
         <div
           className={` transition-colors duration-300 ${
             !isCelsius ? "text-white" : "text-sky-600"
           }`}
         >
-          C<span>&#176;</span>
+          C
         </div>
         <div
           className={` transition-colors duration-300 ${
             isCelsius ? "text-white" : "text-sky-600"
           }`}
         >
-          F<span>&#176;</span>
+          F
         </div>
       </div>
     </div>
--- src/components/WeatherAppUIContainer.tsx
+++ src/components/WeatherAppUIContainer.tsx
@@ -7,30 +7,30 @@ import { GlobalContext } from "../store/store";
 import useInitalizeApp from "../hooks/useInitalizeApp";
 
 export default function WeatherAppUIContainer() {
-  useInitalizeApp();
   const { setShowSearchPopup, selectedCity } = useContext(GlobalContext);
   const [isCelsius, setIsCelsius] = useState(false);
+  useInitalizeApp();
 
   return (
     <div className="max-w-[670px]">
       <div className="shadow-2xl relative bg-[#D9FBFF] mx-2">
-        <div className=" absolute top-0 px-3 py-2 left-0 w-full flex flex-col sm:flex-row justify-between">
+        <div className=" absolute top-0 px-3 py-2 left-0 w-full flex flex-row justify-between">
           <TodayWeatherDisplay
             selectedCity={selectedCity}
             isCelsius={isCelsius}
           />
           <TempToggle isCelsius={isCelsius} setIsCelsius={setIsCelsius} />
         </div>
-        <div className="sm:pt-0 xs:pt-20 pt-44">
+        <div className="sm:pt-0 xs:pt-20 pt-32">
           <img src="/assets/Dallas.png" />
         </div>
         <img
           src="/assets/clouds.svg"
-          className="absolute hidden xs:block top-16 -right-20 clouds-animation-1"
+          className="absolute hidden xs:block top-16 z-40 -right-20 clouds-animation-1"
         />
         <img
           src="/assets/clouds-2.svg"
-          className="absolute  hidden xs:block top-24 -left-16 clouds-animation-2"
+          className="absolute  hidden xs:block top-24 z-40  -left-16 clouds-animation-2"
         />
         <WeeklyWeatherDisplay
           selectedCity={selectedCity}
